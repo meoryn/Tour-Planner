4bd3dd3198b2cfa0c281d9207b8c7a95b31b8066
Manuel Elmer <elmer.manuel00@gmail.com>
2026-04-07 19:18:00 +0200

    added routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,5 +8,17 @@ export const routes: Routes = [
     {
         path: 'login',
         loadComponent: () => import('./login/login').then(m => m.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./register/register').then(m => m.RegisterComponent)
+    },
+    {
+        path: 'tour/:id/edit',
+        loadComponent: () => import('./edit-tour/edit-tour').then(m => m.EditTourComponent)
+    },
+    {
+        path: 'tour/add',
+        loadComponent: () => import('./add-tour/add-tour').then(m => m.AddTourComponent)
     }
 ];
