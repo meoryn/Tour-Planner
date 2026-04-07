c10ea0a50c7856e042db5327b049a2334946d686
Manuel Elmer <elmer.manuel00@gmail.com>
2026-04-07 15:07:52 +0200

    added basic state services

---
+++ frontend/src/app/tour-state-service.ts
@@ -0,0 +1,60 @@
+import { Injectable, computed, effect, inject, signal } from '@angular/core';
+import { UserStateService } from './user-state-service';
+
+export enum TransportType {
+  Walk = 'walk',
+  Car = 'car',
+  Train = 'train'
+}
+
+export type MapCoordinates = {
+    lat: number;
+    lng: number;
+}
+
+export type TourLog = {
+    id: number;
+    creationDate: Date;
+    description: string;
+    rating: number;
+}
+
+export type Tour = {
+    transportType: TransportType;
+    totalDistance: number;
+    totalDuration: number;
+    from: MapCoordinates;
+    to: MapCoordinates;
+    tourName: string;
+    description: string;
+    logs? : TourLog[];
+    creatorId: number;
+} 
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TourStateService {
+
+    private userStateService = inject(UserStateService);
+
+    private readonly _tours = signal<Tour[]>([]);
+    
+    public tours = this._tours.asReadonly();
+
+    public addTour(tour: Tour) {
+        this._tours.set([...this._tours(), tour]);
+    }
+    
+    public removeTour(tourName: string) {
+        this._tours.set(this._tours().filter(t => t.tourName !== tourName));
+    }
+
+    readonly userTours = computed(() => {
+        const currentUser = this.userStateService.currentUser();
+        if (!currentUser) {
+            return [];
+        }
+        return this._tours().filter(t => t.creatorId === currentUser.id);
+    });
+}
---
+++ frontend/src/app/user-state-service.ts
@@ -0,0 +1,35 @@
+import { Injectable, computed, effect, inject, signal } from '@angular/core';
+
+export type User = {
+  id: number;
+  email: string;
+  password: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserStateService {
+  private readonly _users = signal<User[]>([]);
+  private readonly _currentUser = signal<User | null>(null);
+
+  public users = this._users.asReadonly();
+  public currentUser = this._currentUser.asReadonly();
+
+  addUser(user: User) {
+    this._users.set([...this._users(), user]);
+  }
+
+  removeUser(email: string) {
+    this._users.set(this._users().filter(u => u.email !== email));
+  }
+
+  loginUser(email: string, password: string) {
+    const user = this._users().find(u => u.email === email && u.password === password);
+    if (user) {
+      this._currentUser.set(user);
+    } else {
+      //TODO: Handle login failure (e.g., show error message)
+    }
+  }
+}
