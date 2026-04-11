cd7792f39de9ed8ee25550ce68da663100b62fc1
Manuel Elmer <elmer.manuel00@gmail.com>
2026-04-11 14:12:41 +0200

    added icon package, added mobile view for tour list

--- frontend/src/app/navbar/navbar.ts
+++ frontend/src/app/navbar/navbar.ts
@@ -1,13 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from "@angular/router";
+import { Drawer } from 'primeng/drawer';
 
 @Component({
     selector: "app-navbar",
     templateUrl: "./navbar.html",
     styleUrl: "./navbar.css",
-    imports: [ButtonModule, RouterLink],
+    imports: [ButtonModule, RouterLink, Drawer],
     standalone: true
 })
 export class NavbarComponent {
+    drawerVisible = signal(false);
 }
