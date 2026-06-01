2db7ecde8ec83b6dfe0119b8c2ca2df6cb3c0e6c
maximilianjandl <jandl.maximilian@gmail.com>
2026-06-01 15:47:15 +0200

    added leaflet

---
+++ frontend/src/app/map/map-facade.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import type * as L from 'leaflet';
+import { MapCoordinates } from '../tour-state-service';
+
+
+@Injectable()
+export class MapFacade {
+  private center: MapCoordinates = { lat: 48.2082, lng: 16.3738 };
+  private zoom = 6;
+
+  private map?: L.Map;
+
+  async init(element: HTMLElement, center = this.center): Promise<void> {
+    const leaflet = await import('leaflet');
+
+    this.map = leaflet.map(element).setView([center.lat, center.lng], this.zoom);
+
+    leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    this.map.invalidateSize();
+  }
+
+  destroy(): void {
+    this.map?.remove();
+    this.map = undefined;
+  }
+}
