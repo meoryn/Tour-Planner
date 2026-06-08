c4456ba275b7e06dd117aa74939af9fe46efe6dc
Manuel Elmer <elmer.manuel00@gmail.com>
2026-06-08 21:44:31 +0200

    added custom user details service

---
+++ backend/src/main/java/at/fhtw/backend/service/TourUserDetailsService.java
@@ -0,0 +1,28 @@
+package at.fhtw.backend.service;
+import at.fhtw.backend.model.entities.User;
+import org.springframework.security.core.userdetails.*;
+import at.fhtw.backend.persistence.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+
+@Service
+public class TourUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public TourUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
